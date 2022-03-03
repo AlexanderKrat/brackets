e848d2d60654f9cc4ab6c675ab30d4fa1991cc4b
Alexander <alexanderkratexor@gmail.com>
2022-03-03 18:58:26 +0200

    docs: update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+ let stack = [];
+ let pairs = {
+   ['}'] : '{',
+   [')'] : '(',
+   ['|'] : '|',
+   [']'] : '['
+ }
+ for (let i=0; i < str.length ; i++){
+   let curr = str[i];
+   bracketsConfig.forEach((el) => {
+     if (el[0] === curr){
+       stack.push(curr)
+       if(pairs[curr] === curr){
+         stack.pop()
+       }
+     }
+     else{
+       let top = stack[stack.length -1]
+       if(stack.length === 0){
+         return false;
+       }
+       else{
+         if(pairs[curr] === top){
+           stack.pop()
+         }
+         else{
+           return false;
+         }
+       }
+     }
+   })
+ }
+ return stack.length === 0;
 }
+
